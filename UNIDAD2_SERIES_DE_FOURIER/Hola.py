1429fd9b3a4f2a28149f5261cc4c7893d8c7e183
SANDY332211 <san2040513@gmail.com>
2025-10-13 14:18:24 -0600

    Primer repositorio para guardar elementos agregados

---
+++ UNIDAD2_SERIES_DE_FOURIER/Hola.py
@@ -0,0 +1,27 @@
+# ITESS TICS grupo 501
+# Analisis de señales y sistemas de comunicacion
+# Docente Fransisco Javier Montesillo Puente
+# Programador Sandra Araceli Hernandez Muñoz
+# 09/10/2025
+print ("Hola Mundo")
+# Usar python como calculadora 
+
+#Buscar como se concatena, los for, if y estructuras como arreglos, diccionari etc
+#suma 
+print ("suma")
+print (2+2)
+#Multiplicacion 
+print ("multiplicacion")
+print (100*4)
+#una operacion similar a la multiplicacion pero con dos * es equivalente a exponente 
+print ("exponente")
+print (2**4)
+#dicivivoon con una sola / y condoble //
+print ("divicion imprime decimal")
+print (200/4)
+print ("Divicion que imprime entero")
+print (200//4)
+#Modulo % es lo que sobra de una divicion 
+print ("modulo")
+print (50%2)
+
