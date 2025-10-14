4c15203d41f720d829b36189a0455d35f22d94d8
SANDY332211 <san2040513@gmail.com>
2025-10-13 21:20:09 -0600

    Programa de Listas en python

--- UNIDAD2_SERIES_DE_FOURIER/Hola.py
+++ UNIDAD2_SERIES_DE_FOURIER/Hola.py
@@ -4,24 +4,45 @@
 # Programador Sandra Araceli Hernandez Muñoz
 # 09/10/2025
 print ("Hola Mundo")
-# Usar python como calculadora 
 
-#Buscar como se concatena, los for, if y estructuras como arreglos, diccionari etc
-#suma 
+# 1 Usar python como calculadora/ Numeros
+# suma 
 print ("suma")
 print (2+2)
+
 #Multiplicacion 
 print ("multiplicacion")
 print (100*4)
+
 #una operacion similar a la multiplicacion pero con dos * es equivalente a exponente 
 print ("exponente")
 print (2**4)
-#dicivivoon con una sola / y condoble //
+
+# la división con una / simpre retorna un número de punto flotante
 print ("divicion imprime decimal")
 print (200/4)
+
+# la división entera descarta la parte fraccional
 print ("Divicion que imprime entero")
 print (200//4)
-#Modulo % es lo que sobra de una divicion 
+
+# Modulo % retorna el resto de la división
 print ("modulo")
-print (50%2)
+print (50 % 3)
+
+# El signo igual (=) es usado para asignar un valor a una variable
+ancho = 20
+largo = 5 * 9
+ancho * largo
+
+# Asignar a la variable _ Esto significa que cuando estés usando Python como una calculadora de escritorio, es más fácil seguir calculando
+impuesto = 12.5 / 100
+precio = 100.50
+precio * impuesto
+precio + _
+round(_, 2)
+
+
+
+
 
---
+++ UNIDAD2_SERIES_DE_FOURIER/Listas.py
@@ -0,0 +1,63 @@
+# ITESS TICS grupo 501
+# Analisis de señales y sistemas de comunicacion
+# Docente Fransisco Javier Montesillo Puente
+# Programador Sandra Araceli Hernandez Muñoz
+# 13/10/2025
+
+# Listas 
+#Una lista en Python se define usando corchetes [ ]  
+cuadrados = [1, 4, 9, 16, 25]
+print(cuadrados)
+
+# Acceder a elementos por índice
+print(cuadrados[0]) #índice 0 es el primer elemento
+print(cuadrados[-1]) # índice -1 es el último elemento
+print(cuadrados[-3:]) #rebanada que retorna los ultimos 3 elementos 
+
+# Crear una copia de la lista, devuleve una copia superfisial de toda la lista 
+nueva_lista = cuadrados[:]
+print(nueva_lista)
+
+#Unir o concatenar dos listas 
+nueva_lista = cuadrados + [36, 49, 64, 81, 100]
+print(nueva_lista)
+
+#modificar elementos de una lista 
+# Lista con un error
+cubos = [1, 8, 27, 65, 125]
+print("Antes:", cubos)
+# Corregir el valor erróneo en el elemento 3
+cubos[3] = 64
+print("Después:", cubos)
+
+#Agregar elementos al final (append)
+cubos.append(216)    
+cubos.append(7 ** 3) 
+print(cubos)
+
+#Reemplazar y eliminar elementos con slices
+letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print("Original:", letras)
+# Reemplazar algunos valores
+letras[2:5] = ['C', 'D', 'E']
+print("Reemplazado:", letras)
+# Eliminar esos valores
+letras[2:5] = []
+print("Eliminado:", letras)
+# Vaciar toda la lista
+letras[:] = []
+print("Lista vacía:", letras)
+
+#Calcular la longitud de una lista con len
+letras = ['a', 'b', 'c', 'd']
+print(len(letras))
+
+#Listas anidadas
+a = ['a', 'b', 'c']
+n = [1, 2, 3]
+x = [a, n]
+print(x)        # muestra la lista anidada
+print(x[0])     # muestra la primera sublista
+print(x[0][1])  # muestra el segundo elemento de la primera sublista
+
+
